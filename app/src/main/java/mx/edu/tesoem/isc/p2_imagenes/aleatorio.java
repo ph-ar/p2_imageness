c60b9847bcb30c554bdd0daf6d35856fe45f7b9a
JM-rich <richard.maciel1397@gmailcom>
2020-04-05 20:49:07 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p2_imagenes/aleatorio.java
@@ -0,0 +1,18 @@
+package mx.edu.tesoem.isc.p2_imagenes;
+
+public class aleatorio {
+    int Imagen;
+    String aleatorio;
+    public aleatorio(int Imagen,String aleatorio){
+        this.Imagen = Imagen;
+        this.aleatorio = aleatorio;
+    }
+
+    public int getImagen() {
+        return Imagen;
+    }
+
+    public String getAleatorio() {
+        return aleatorio;
+    }
+}
